134aa2352436c7638918765bc7835cef9143cb39
MishraLalit2024 <lalit.mishra2024dev@gmail.com>
2024-04-22 15:37:11 +0530

    Changed the logic and view for education details.

--- Form/public/basic.js
+++ Form/public/basic.js
@@ -43,180 +43,180 @@ function loadState() {
 }
 
 
-//Function to validate for names (Only alphabets fields);
-function checkNames(regex, handler, flag) {
-    document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
-        e.preventDefault();
-        document.getElementById(`message-${handler}`).innerText = 'Only Alphabets are valid.';
-    })
-    document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
-        e.preventDefault();
-        if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
-            document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
-        }
-        else if (!regex.test(e.target.value)) {
-            document.getElementById(`message-${handler}`).innerText = 'Please fill alphabets only';
-        }
-        else {
-            document.getElementById(`message-${handler}`).innerText = '';
-        }
-    })
-    flag = false;
-}
+// //Function to validate for names (Only alphabets fields);
+// function checkNames(regex, handler, flag) {
+//     document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
+//         e.preventDefault();
+//         document.getElementById(`message-${handler}`).innerText = 'Only Alphabets are valid.';
+//     })
+//     document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
+//         e.preventDefault();
+//         if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
+//             document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
+//         }
+//         else if (!regex.test(e.target.value)) {
+//             document.getElementById(`message-${handler}`).innerText = 'Please fill alphabets only';
+//         }
+//         else {
+//             document.getElementById(`message-${handler}`).innerText = '';
+//         }
+//     })
+//     flag = false;
+// }
 
-//Function for E-MAIL Validation:
-function checkEmail(regex, handler, flag) {
-    document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
-        e.preventDefault();
-        document.getElementById(`message-${handler}`).innerText = 'Enter a valid email.';
-    })
-    document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
-        e.preventDefault();
-        if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
-            document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
-        }
-        else if (!regex.test(e.target.value)) {
-            document.getElementById(`message-${handler}`).innerText = 'Not Valid E-mail address';
-        }
-        else {
-            document.getElementById(`message-${handler}`).innerText = '';
-        }
-    })
-    flag = false;
-}
+// //Function for E-MAIL Validation:
+// function checkEmail(regex, handler, flag) {
+//     document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
+//         e.preventDefault();
+//         document.getElementById(`message-${handler}`).innerText = 'Enter a valid email.';
+//     })
+//     document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
+//         e.preventDefault();
+//         if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
+//             document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
+//         }
+//         else if (!regex.test(e.target.value)) {
+//             document.getElementById(`message-${handler}`).innerText = 'Not Valid E-mail address';
+//         }
+//         else {
+//             document.getElementById(`message-${handler}`).innerText = '';
+//         }
+//     })
+//     flag = false;
+// }
 
-//Function to validate phone number:
-function checkPhone(regex, handler, flag) {
-    document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
-        e.preventDefault();
-        document.getElementById(`message-${handler}`).innerText = 'In the format +91 XXXXX XXXXX';
-    })
-    document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
-        e.preventDefault();
-        if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
-            document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
-        }
-        else if (!regex.test(e.target.value)) {
-            document.getElementById(`message-${handler}`).innerText = 'Not Valid Mobile Number';
-        }
-        else {
-            document.getElementById(`message-${handler}`).innerText = '';
-        }
-    })
-    flag = false;
-}
+// //Function to validate phone number:
+// function checkPhone(regex, handler, flag) {
+//     document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
+//         e.preventDefault();
+//         document.getElementById(`message-${handler}`).innerText = 'In the format +91 XXXXX XXXXX';
+//     })
+//     document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
+//         e.preventDefault();
+//         if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
+//             document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
+//         }
+//         else if (!regex.test(e.target.value)) {
+//             document.getElementById(`message-${handler}`).innerText = 'Not Valid Mobile Number';
+//         }
+//         else {
+//             document.getElementById(`message-${handler}`).innerText = '';
+//         }
+//     })
+//     flag = false;
+// }
 
-//Functions to validate rest of the input[type=text] fields:
-function checkInput(regex, handler, flag) {
-    document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
-        e.preventDefault();
-        document.getElementById(`message-${handler}`).innerText = '';
-    })
-    document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
-        e.preventDefault();
-        if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
-            document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
-        }
-        else if (!regex.test(e.target.value)) {
-            document.getElementById(`message-${handler}`).innerText = 'Not Valid';
-        }
-        else {
-            document.getElementById(`message-${handler}`).innerText = '';
-        }
-    })
-    flag = false
-}
-
-
+// //Functions to validate rest of the input[type=text] fields:
+// function checkInput(regex, handler, flag) {
+//     document.getElementById(`handle-${handler}`).addEventListener("focusin", (e) => {
+//         e.preventDefault();
+//         document.getElementById(`message-${handler}`).innerText = '';
+//     })
+//     document.getElementById(`handle-${handler}`).addEventListener("focusout", (e) => {
+//         e.preventDefault();
+//         if (e.target.value === undefined || e.target.value === '' || e.target.value === null) {
+//             document.getElementById(`message-${handler}`).innerText = 'Cannot be left empty.';
+//         }
+//         else if (!regex.test(e.target.value)) {
+//             document.getElementById(`message-${handler}`).innerText = 'Not Valid';
+//         }
+//         else {
+//             document.getElementById(`message-${handler}`).innerText = '';
+//         }
+//     })
+//     flag = false
+// }
 
 
 
-//Validations for the fields in the basic details phase;
-var flag;
-
-//Regex:
-let nameRegex = /^[A-Za-z ]+$/;
-let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-let phoneRegex = /^[+][91]{1,3}[ ]{1}[0-9]{1,5}[ ]{1}[0-9]{1,5}/;
-let normRegex = /^[A-Za-z0-9 ,-/]+$/;
-let pincodeRegex = /^[0-9]{1,6}/;
-
-//Validation function calls:
-checkNames(nameRegex, "1", flag);
-checkNames(nameRegex, "2", flag);
-checkNames(nameRegex, "3", flag);
-checkEmail(emailRegex, "4", flag);
-checkPhone(phoneRegex, "5", flag);
-checkInput(normRegex, "8", flag);
-checkInput(normRegex, "9", flag);
-checkInput(pincodeRegex, "12", flag);
 
 
+// //Validations for the fields in the basic details phase;
+// var flag;
 
-//Now validations are done, so we will preview and save the data:
-document.getElementById("saveBasic").addEventListener("click", (e) => {
-    flag = false;
-    e.preventDefault();
-    for (let i = 1; i <= 13; i++) {
-        if (document.getElementById(`message-${i}`).innerText == '') {
-            flag = true;
-        }
-        else {
-            flag = false;
-        }
-    }
-    let phone = document.getElementById("handle-5").value;
-    if (phone.length == 15) {
-        flag = true;
-    }
-    else {
-        flag = false;
-        document.getElementById("message-5").innerText = "Not a Valid Number"
-    }
-    let pin = document.getElementById("handle-12").value;
-    if (pin.length == 6) {
-        flag = true;
-    }
-    else {
-        flag = false;
-        document.getElementById("message-12").innerText = "Not a Valid PIN Code"
-    }
-    let genderFlag = false;
-    const gender = document.getElementsByName("gender");
-    gender.forEach(el => {
-        if (el.checked){
-            genderFlag = true;
-        }
-    })
-    if(genderFlag===false){
-        document.getElementById("message-6").innerText = "Gender cannot be empty"
-    }
-    else{
-        document.getElementById("message-6").innerText = ""
-    }
+// //Regex:
+// let nameRegex = /^[A-Za-z ]+$/;
+// let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+// let phoneRegex = /^[+][91]{1,3}[ ]{1}[0-9]{1,5}[ ]{1}[0-9]{1,5}/;
+// let normRegex = /^[A-Za-z0-9 ,-/]+$/;
+// let pincodeRegex = /^[0-9]{1,6}/;
 
-    flag = genderFlag;
+// //Validation function calls:
+// checkNames(nameRegex, "1", flag);
+// checkNames(nameRegex, "2", flag);
+// checkNames(nameRegex, "3", flag);
+// checkEmail(emailRegex, "4", flag);
+// checkPhone(phoneRegex, "5", flag);
+// checkInput(normRegex, "8", flag);
+// checkInput(normRegex, "9", flag);
+// checkInput(pincodeRegex, "12", flag);
 
-    console.log(flag);
 
-    if (flag == true) {
-        document.getElementById("nextBasic").removeAttribute("disabled", "true");
-        let parent = document.getElementById("basic-class");
-        let input = parent.querySelectorAll("input");
-        let text = parent.querySelectorAll("textarea");
-        let select = parent.querySelectorAll("select");
-        input.forEach(x => {
-            x.disabled = !x.disabled;
-        })
-        text.forEach(el => {
-            el.disabled = !el.disabled;
-        })
-        select.forEach(el =>{
-            el.disabled = !el.disabled;
-        })
-        document.getElementById("nextBasic").style.backgroundColor = 'green';
-    }
-    else {
-        document.getElementById("nextBasic").setAttribute("disabled", "true");
-    }
-})
+
+// //Now validations are done, so we will preview and save the data:
+// document.getElementById("saveBasic").addEventListener("click", (e) => {
+//     flag = false;
+//     e.preventDefault();
+//     for (let i = 1; i <= 13; i++) {
+//         if (document.getElementById(`message-${i}`).innerText == '') {
+//             flag = true;
+//         }
+//         else {
+//             flag = false;
+//         }
+//     }
+//     let phone = document.getElementById("handle-5").value;
+//     if (phone.length == 15) {
+//         flag = true;
+//     }
+//     else {
+//         flag = false;
+//         document.getElementById("message-5").innerText = "Not a Valid Number"
+//     }
+//     let pin = document.getElementById("handle-12").value;
+//     if (pin.length == 6) {
+//         flag = true;
+//     }
+//     else {
+//         flag = false;
+//         document.getElementById("message-12").innerText = "Not a Valid PIN Code"
+//     }
+//     let genderFlag = false;
+//     const gender = document.getElementsByName("gender");
+//     gender.forEach(el => {
+//         if (el.checked){
+//             genderFlag = true;
+//         }
+//     })
+//     if(genderFlag===false){
+//         document.getElementById("message-6").innerText = "Gender cannot be empty"
+//     }
+//     else{
+//         document.getElementById("message-6").innerText = ""
+//     }
+
+//     flag = genderFlag;
+
+//     console.log(flag);
+
+//     if (flag == true) {
+//         document.getElementById("nextBasic").removeAttribute("disabled", "true");
+//         let parent = document.getElementById("basic-class");
+//         let input = parent.querySelectorAll("input");
+//         let text = parent.querySelectorAll("textarea");
+//         let select = parent.querySelectorAll("select");
+//         input.forEach(x => {
+//             x.disabled = !x.disabled;
+//         })
+//         text.forEach(el => {
+//             el.disabled = !el.disabled;
+//         })
+//         select.forEach(el =>{
+//             el.disabled = !el.disabled;
+//         })
+//         document.getElementById("nextBasic").style.backgroundColor = 'green';
+//     }
+//     else {
+//         document.getElementById("nextBasic").setAttribute("disabled", "true");
+//     }
+// })
--- Form/public/education.js
+++ Form/public/education.js
@@ -7,55 +7,72 @@
  */
 
 
-function showEdu(){
-    document.getElementById('education').style.display="block";
+function showEdu() {
+    document.getElementById('education').style.display = "block";
 }
-function hideEdu(){
-    document.getElementById('education').style.display="none";
+function hideEdu() {
+    document.getElementById('education').style.display = "none";
 }
 
 
 //Function to add the rows in the education fields:
-let rowCount=1;
-function addEduRow(){
-    if(rowCount<0){     //To verify whether the rowCount is negative or not.
-        rowCount=1
+let rowCount = 1;
+function addEduRow() {
+    if (rowCount < 0) {     //To verify whether the rowCount is negative or not.
+        rowCount = 1
     }
-    else if(rowCount<7){//To limit the number of rows
-        str = `<div class="flexer" id="edu-row-${rowCount}">
-                    Course: <select name="course-${rowCount}" id="handle-14-${rowCount}" onchange="activeField(${rowCount})">
-                        <option value="ssc">SSC</option>
-                        <option value="hsc">HSC</option>
-                        <option value="dip">Diploma</option>
-                        <option value="bac">Bachelor</option>
-                        <option value="mas">Master</option>
-                        <option value="doc">Doctorate</option>
-                    </select>
-                        <label for="pYear">Passing Year: </label>
-                    <input type="text" name="pyear-${rowCount}" id="handle-15-${rowCount}" disabled>
-                        <label for="score">Percentage: </label>
-                    <input type="text" name="score-${rowCount}" id="handle-16-${rowCount}" disabled>
-                </div>`
+    else if (rowCount < 7) {//To limit the number of rows
+        str = `<div id="edu-row-${rowCount}" class="flexer">
+        <div>
+          Course:
+          <select name="course-${rowCount}" id="handle-14-${rowCount}" onchange="activeField(${rowCount})">
+            <option value="ssc">SSC</option>
+            <option value="hsc">HSC</option>
+            <option value="dip">Diploma</option>
+            <option value="bac">Bachelor</option>
+            <option value="mas">Master</option>
+            <option value="doc">Doctorate</option>
+          </select>
+        </div>
+        <div class="flexer">
+          <input
+          class="edu-spy"
+            type="text"
+            name="pyear-${rowCount}"
+            id="handle-15-${rowCount}"
+            placeholder="Passing Year"
+          />
+          <input
+          class="edu-spy"
+            type="text"
+            name="score-${rowCount}"
+            id="handle-16-${rowCount}"
+            placeholder="Score"
+          />
+          </div>
+          <div class="edu-inputs">
+          <input type="button" value="Remove" onclick="remEdu(${rowCount})" />
+        </div>`
         document.getElementById('edu-row').innerHTML += str;
         rowCount++;
     }
 }
 
 //Function to remove the rows from the education fields:
-function remEdu(){
-    if(rowCount > 1){   //to stop user from deleting all the rows
-        parent = document.getElementById('edu-row');
-        parent.removeChild(parent.lastChild);
+function remEdu(data) {
+    if (rowCount > 1) {   //to stop user from deleting all the rows
+        parent = document.getElementById(`edu-row-${data}`);
+        parent.remove();
         rowCount--;
     }
-    else{
+    else {
         return;
     }
 }
 
 
 //to active all the fields on change of option:
-function activeField(num){
+function activeField(num) {
     document.getElementById(`handle-15-${num}`).removeAttribute("disabled", "true");
     document.getElementById(`handle-16-${num}`).removeAttribute("disabled", "true");
 }
